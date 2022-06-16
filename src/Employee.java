1bc9fb0c0098034baca654f11e8db94c89565d8e
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-06-16 20:23:04 +0300

    Курсовая работа

--- src/Employee.java
+++ src/Employee.java
@@ -1,83 +1,48 @@
 public class Employee {
-    private String firstName;
-    private String middleName;
-    private String lastName;
-    private String salary;
-    private String department1;
-    private String department2;
-    private String department3;
-    private String department4;
-    private String department5;
-    static int id;
+    public String staff;
 
-    public Employee(String firstName, String middleName, String lastName, String salary, String department1, String department2, String department3, String department4, String department5) {
-        this.firstName = firstName;
-        this.middleName = middleName;
-        this.lastName = lastName;
-        this.salary = salary;
-        this.department1 = department1;
-        this.department2 = department2;
-        this.department3 = department3;
-        this.department4 = department4;
-        this.department5 = department5;
-    }
+    public int salaryStaff;
+
+    public int departmentNumber;
+
+    public static int id = 0;
 
 
-    public String getFirstName() {
-        return firstName;
+    public Employee(String staff, int salaryStaff, int departmentNumber) {
+        this.staff = staff;
+        this.salaryStaff = salaryStaff;
+        this.departmentNumber = departmentNumber;
+        this.id = id;
+        if (id > 0) {
+            id++;
+        }
     }
 
-    public String getMiddleName() {
-        return middleName;
+    public String getStaff() {
+        return staff;
     }
 
-    public String getLastName() {
-        return lastName;
+    public int getSalaryStaff() {
+        return salaryStaff;
     }
 
-    public String getSalary() {
-        return salary;
+    public int getDepartmentNumber() {
+        return departmentNumber;
     }
 
-    public String getDepartment1() {
-        return department1;
+    public void setDepartmentNumber() {
+        this.departmentNumber = departmentNumber;
     }
 
-    public String getDepartment2() {
-        return department2;
-    }
-
-    public String getDepartment3() {
-        return department3;
-    }
-
-    public String getDepartment4() {
-        return department4;
-    }
-
-    public String getDepartment5() {
-        return department5;
-    }
-
-    public void setSalary() {
-        this.salary = salary;
-    }
-
-    public void setDepartment1() {
-        this.department1 = department1;
-    }
-    public void setDepartment2() {
-        this.department2 = department2;
-    }
-    public void setDepartment3() {
-        this.department3 = department3;
-    }
-    public void setDepartment4() {
-        this.department4 = department4;
-    }
-    public void setDepartment5() {
-        this.department5 = department5;
+    public void setSalaryStaff() {
+        this.salaryStaff = salaryStaff;
     }
 }
+
+
+
+
+
+
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int[] Employee = new int[10];
+        Employee[] employee = new Employee[10];
+        employee[0] = new Employee(" Ivanov Ivan Ivanovich ", 55000, 1);
+        employee[1] = new Employee(" Makarov Makar Makarovich ", 65000, 2);
+        employee[2] = new Employee(" Maksimov Maksim Maksimovich ", 99000, 3);
+        employee[3] = new Employee(" Vasilkov Visaliy Vasilylievich ", 98000, 4);
+        employee[4] = new Employee(" Bykin Gennadi Gennadievich ", 120000, 5);
+        employee[5] = new Employee(" Galkin Maksim Maksimovich ", 130000, 1);
+        employee[6] = new Employee(" Pygacheva Nadezda Valerievna ", 200000, 2);
+        employee[7] = new Employee(" Kirkorov Vladimir Ostapovich ", 33000, 3);
+        employee[8] = new Employee(" Kirienko Viktor Viktorovich ", 78000, 4);
+        employee[9] = new Employee(" Medvedev Dmitriy Ivanovich ", 140000, 5);
+        for (int i = 0; i < employee.length; i++) {
+            System.out.println(employee[i]);
+        }
     }
 }
